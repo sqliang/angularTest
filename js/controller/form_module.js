b1136bb399c98ee95f50b0d68d6d4015d8855c29
sqliang <1053196988@qq.com>
2015-10-08 19:13:53 +0800

    sqliang

--- js/controller/form_module.js
+++ js/controller/form_module.js
@@ -1,3 +1,20 @@
 /**
  * Created by sqlliang on 2015/10/3.
  */
+var form_module = angular.module('form_module',[]);
+
+form_module.controller('form_ctrl', function ($scope) {
+    $scope.master = {
+        firstName : 'John',
+        lastName : 'Doe'
+    };
+    $scope.reset = function(){
+        $scope.user = angular.copy($scope.master);
+    };
+    $scope.reset();
+});
+
+form_module.controller('validate_ctrl', function($scope) {
+    $scope.user = 'John Doe';
+    $scope.email = 'john.doe@gmail.com';
+});
